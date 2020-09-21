a91399e3e5a98a2f8ee1cfa62a2955a35b8962f5
Inon Bercu <inonbr@gmail.com>
2020-09-21 13:46:37 +0300

    get response from server

--- player.js
+++ player.js
@@ -1,5 +1,14 @@
-const hi = () => {
-  console.log('hello world!');
-};
+$.ajax({
+  url:
+    'https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&amp;refresh=true&amp;env=dev&amp;type=startPanel&amp;vars%5Btype%5D=startPanel&amp;sid=none&amp;_=1582203987867',
+  jsonp: 'callback',
+  dataType: 'jsonp',
+  data: {
+    format: 'json',
+  },
 
-hi();
+  // get response from server
+  success: function (response) {
+    console.log(response);
+  },
+});
