a2de9343fc139663ce668d153c324fc140bf4478
Inon Bercu <inonbr@gmail.com>
2020-09-22 11:49:43 +0300

    read and use css from JSON

--- player.js
+++ player.js
@@ -1,3 +1,5 @@
+// @import
+
 $.ajax({
   url:
     'https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&amp;refresh=true&amp;env=dev&amp;type=startPanel&amp;vars%5Btype%5D=startPanel&amp;sid=none&amp;_=1582203987867',
@@ -9,6 +11,15 @@ $.ajax({
 
   // get response from server
   success: function (response) {
-    console.log(response);
+    let selector = document.querySelector(
+      response.data.structure.steps[0].action.selector
+    );
+
+    // read css and call it for use
+    const css_styles = response.data.css;
+    let link = document.createElement('link');
+    link.rel = 'linksheet';
+    link.type = 'text/css';
+    link.href = css_styles;
   },
 });
