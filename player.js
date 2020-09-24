7f1c12e9fe3b131f595dbfcd1e06f3f20bca065a
Inon Bercu <inonbr@gmail.com>
2020-09-24 11:14:10 +0300

    exit button function done

--- player.js
+++ player.js
@@ -12,22 +12,31 @@ $.ajax({
     const selector = response.data.structure.steps;
 
     // read css and call it for use (head)
-    let css_style = response.data.css;
-    const style = $(`<style>${css_style}</style>`);
+    const cssStyle = response.data.css;
+    const style = $(`<style>${cssStyle}</style>`);
     $('head')[0].appendChild(style[0]);
 
     // read tip HTML and call it for use (body)
-    let tip = response.data.tiplates['tip'];
-    const tipElement = $(`<div class=tip-div>${tip}</div>`);
+    const tip = response.data.tiplates['tip'];
+    const tipElement = $(
+      `<div class="tip-div" id="tip-div-wrapper">${tip}</div>`
+    );
     $('body').prepend(tipElement[0]);
 
-    // select element from tip (to edit)
+    // set all types of buttons
+    const backButton = $('#tip-div-wrapper').find('.prev-btn')[1];
+    const nextButton = $('#tip-div-wrapper').find('.next-btn')[0];
+    const remindMeLaterButton = $('#tip-div-wrapper').find('.prev-btn')[0];
+    const exitButton = $('#tip-div-wrapper')
+      .find('.popover-title')
+      .children()[0];
 
-    // select close btn and create event
-    let closeBtb = tipElement.find('.popover-title').children()[0];
-    console.log(closeBtb);
+    // set exit button function
+    $(exitButton).click(function () {
+      $('#tip-div-wrapper').css('display', 'none');
+    });
 
-    // set steps (1/5 for axample)
+    // set steps (1/5)
     let stepElement = tipElement.find('.steps-count').children();
     stepElement.html(response.data.structure.steps[0].action.stepOrdinal);
     $('.steps-count span:nth-child(2)').html(selector.length);
