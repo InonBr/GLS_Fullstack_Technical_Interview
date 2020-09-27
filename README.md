61ce31871532af7dc4609323859db8edbb42e4f3
Inon Bercu <inonbr@gmail.com>
2020-09-27 19:59:35 +0300

    added error function if JSON didn't load successfully + edited README file

--- README.md
+++ README.md
@@ -2,16 +2,20 @@
 
 ## How to use
 
-- Install the "Custom JavaScript for websites" (CJS) addon Google Chrome (https://chrome.google.com/webstore/detail/custom-javascript-for-web/poakhlngfciodnhlhhgnaaelnpjljija?hl=en).
+- Install the "Custom JavaScript for websites" (CJS) addon in Google Chrome (https://chrome.google.com/webstore/detail/custom-javascript-for-web/poakhlngfciodnhlhhgnaaelnpjljija?hl=en).
 
 - Copy the code from the file "player.js".
 
-- Paste the code in the code into the CJS text editor.
+- Go to "google.com" and open CJS.
 
-- Chack the box "enable cjs for this host".
+- Check the box "enable cjs for this host".
 
-- In the "You can inject your own external scripts or predefined one:", select "jQuery 2.1.0".
+- In the dropdown menu, after "You can inject your own external scripts or predefined one:", select "jQuery 2.1.0".
+
+- Paste the code into the CJS text editor.
 
 - The program will now run auotomaticlly each time you'll open the "google.com" page.
 
-- If you want to remove the program, simply push the "reset" button in the CJS manue.
+- If you want to remove the program, simply push the "reset" button in the CJS manue and click save.
+
+**If JSON file did not load successfully, open console and check the error massege**
--- player.js
+++ player.js
@@ -1,31 +1,32 @@
 $.ajax({
-  url: './json_file.json',
-  // 'https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&amp;refresh=true&amp;env=dev&amp;type=startPanel&amp;vars%5Btype%5D=startPanel&amp;sid=none&amp;_=1582203987867',
+  url:
+    'https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&amp;refresh=true&amp;env=dev&amp;type=startPanel&amp;vars%5Btype%5D=startPanel&amp;sid=none&amp;_=1582203987867',
   jsonp: 'callback',
   dataType: 'jsonp',
   data: {
     format: 'json',
   },
 
-  // get response from server
+  // get response from the server
   success: function (response) {
     const selector = response.data.structure.steps;
+    console.log('Success: JSON loaded successfully!');
 
     // read css and call it for use (head)
     // const cssStyle = response.data.css;
     // const style = $(`<style>${cssStyle}</style>`);
     // $('head')[0].appendChild(style[0]);
 
-    $('<style>').text(response.data.css).appendTo(document.head);
+    $('<style rel="stylesheet" type="text/css">')
+      .html(response.data.css)
+      .appendTo(document.head);
 
     // read tip HTML and call it for use (body)
     const tip = response.data.tiplates['tip'];
-    const tipElement = $(
-      `<div class="tip-div" id="tip-div-wrapper">${tip}</div>`
-    );
+    const tipElement = $(`<div id="tip-div-wrapper">${tip}</div>`);
     $('body').prepend(tipElement[0]);
 
-    // set all types of buttons
+    // set all buttons
     const backButton = $('#tip-div-wrapper').find('.prev-btn')[1];
     const nextButton = $('#tip-div-wrapper').find('.next-btn')[0];
     const remindMeLaterButton = $('#tip-div-wrapper').find('.prev-btn')[0];
@@ -74,9 +75,13 @@ $.ajax({
       } else {
         $(backButton).css('display', 'block');
       }
+
+      // hide remind me later button and powered by text
+      $(remindMeLaterButton).css('display', 'none');
+      $('#tip-div-wrapper').find('.powered-by').css('display', 'none');
     };
 
-    // setTimeout function for passing steps after "warningTimeout" as passed
+    // setTimeout function for passing steps after "warningTimeout" has passed
     const timedStep = () => {
       if (currentStep < selector.length - 2) {
         t = setTimeout(function () {
@@ -93,7 +98,7 @@ $.ajax({
 
     /* 
         next/back step if user passes Next or Back buttons
-        or next step if "warningTimeout" as passed
+        or next step if "warningTimeout" has passed
     */
 
     // set next button
@@ -112,11 +117,15 @@ $.ajax({
       if (currentStep > 0) {
         currentStep--;
         loadHtml(currentStep);
-        // timedStep();
+        // timedStep(); //
       }
     });
 
     loadHtml();
     timedStep();
+  },
+  error: function (error) {
+    console.log('Error: JSON did not load successfully');
+    console.error(error);
   },
 });
